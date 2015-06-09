accc1e8876ecb0ed6d8370aea85a6ab7a4bac736
gpspake <gpspake@gmail.com>
2015-06-09 00:19:31 -0500

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+#todo
+#install zsh
+#brew install
 
 #setup symlinks
 LINK="ln -s $HOME/dotfiles"
 $LINK/bash/.bash_profile ~/.bash_profile
 $LINK/git/.gitconfig ~/.gitconfig
 $LINK/vim/.vimrc ~/.vimrc
+mkdir -p ~/.oh-my-zsh/plugins/gpspake/gpspake.plugin.zsh
+$LINK/zsh/gpspake.plugin.zsh ~/.oh-my-zsh/plugins/gpspake/gpspake.plugin.zsh
+
+#keybindings

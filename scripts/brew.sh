dd8a96ac80c78b4300a2d91af4f59406b9beef23
George Spake <george@georgespake.com>
2021-11-05 22:11:16 -0500

    export HOMEBREW_BREWFILE and run bundle w verbose

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -4,6 +4,8 @@
 # Homebrew
 # --------
 
+export HOMEBREW_BREWFILE=${DOTFILES_DIRECTORY}/homebrew/Brewfile
+
 # install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -11,4 +13,4 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew tap Homebrew/bundle
 
 # run brew bundle
-brew bundle
+brew bundle -v
